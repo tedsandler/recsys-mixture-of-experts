6c7df62c0a725ab84f467fb0be65ad51fd995eb6
ted sandler <ted@positronix.ai>
2023-11-11 22:50:13 -0800

    update dependencies

--- src/mf.py
+++ src/mf.py
@@ -1,17 +1,18 @@
 
+import numpy as np
 import torch
 from torch import nn, Tensor
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
 
 
 class MatrixFactorization(nn.Module):
-    def __init__(self, n_user, n_item, n_dim=64):
+    def __init__(self, *, n_users, n_items, n_dims=64):
         super().__init__()
-        self.n_user = n_user
-        self.n_item = n_item
-        self.n_dim = n_dim
-        self.U = nn.Embedding(n_user, n_dim, sparse=False)
-        self.V = nn.Embedding(n_item, n_dim, sparse=False)
+        self.n_users = n_users
+        self.n_items = n_items
+        self.n_dims = n_dims
+        self.U = nn.Embedding(n_users, n_dims, sparse=False)
+        self.V = nn.Embedding(n_items, n_dims, sparse=False)
 
     def forward(self, users=None, items=None):
         U = self.U.weight if users is None else self.U(users)
@@ -19,48 +20,23 @@ class MatrixFactorization(nn.Module):
         return (U @ V.T)
 
     def __repr__(self):
-        n_user = self.n_user
-        n_item = self.n_item
-        n_dim = self.n_dim
-        return f"<MatrixFactorization({n_user=}, {n_item=}, {n_dim=})>"
+        n_users = self.n_users
+        n_items = self.n_items
+        n_dims = self.n_dims
+        return f"<MatrixFactorization({n_users=}, {n_items=}, {n_dims=})>"
 
 
-def train(Y: Tensor,
-          model: nn.Module,
-          epochs: int = 10,
-          batch_size = 32,
-          loss_fn: nn.Module = None,
-          optimizer: torch.optim.Optimizer = None,
-          device="cpu") -> None:
-
-    Y = Y.to(device)
-    model = model.to(device)
-
-    if loss_fn is None:
-        loss_fn = nn.CrossEntropyLoss()
+class ExpertNet(nn.Module):
+    def __init__(self, u, V):
+        super().__init__()
+        if isinstance(u, np.ndarray):
+            u = torch.Tensor(u)
+        if isinstance(V, np.ndarray):
+            V = torch.Tensor(V)
+        self.u = nn.parameter.Parameter(u.view(-1,1))
+        self.V = V
     
-    if optimizer is None:
-        optimizer = torch.optim.AdamW(model.parameters(), lr=1e-2, weight_decay=1e-6)
-
-    size = model.n_user
-    dataloader = DataLoader(torch.arange(size), batch_size=batch_size, shuffle=True)
-    model.train()
-    
-    for epoch in range(epochs):
-        batch_loss = 0
-        n_batch = 0
-        for batch, batch_users in enumerate(dataloader):
-            # Compute prediction error
-            logits = model(batch_users)
-            y = Y[batch_users, :]
-            loss = loss_fn(logits, y)
-            # Backpropagation
-            loss.backward()
-            optimizer.step()
-            optimizer.zero_grad()
-            batch_loss += loss.item()
-            n_batch += 1
-        if epoch % 10 == 0:
-            print(f"loss: {batch_loss:>7f} [{epoch=}]") 
-    print(f"loss: {batch_loss:>7f} [{epoch=}]") 
-    return
+    def forward(self, x):
+        n, d = x.shape
+        logits = (self.V @ self.u).T
+        return logits.expand(n, -1)
--- src/mlp.py
+++ src/mlp.py
@@ -25,46 +25,8 @@ class MLP(nn.Module):
     def forward(self, x):
         return self.net(x)
 
-
-def train(dataset: Dataset,
-          model: nn.Module,
-          epochs: int = 10,
-          loss_fn: nn.Module = None,
-          optimizer: torch.optim.Optimizer = None,
-          device = "cpu") -> None:
-
-    if optimizer is None:
-        optimizer = torch.optim.AdamW(model.parameters(), lr=1e-2, weight_decay=1e-6)
-    if loss_fn is None:
-        loss_fn = nn.MSELoss()
-
-    dataloader = DataLoader(dataset, batch_size=100, shuffle=True)
-    size = len(dataloader.dataset)
-    model.train()
-    
-    for epoch in range(epochs):
-        for batch, (X, y) in enumerate(dataloader):
-            X, y = X.to(device), y.to(device)
-            # Compute prediction error
-            pred = model(X)
-            loss = loss_fn(pred, y)
-            # Backpropagation
-            loss.backward()
-            optimizer.step()
-            optimizer.zero_grad()
-            if batch == 0:
-                loss, current = loss.item(), (batch + 1) * len(X)
-                print(f"loss: {loss:>7f} [{epoch=}] [{current:>5d}/{size:>5d}]")        
-
-
-def predict(dataset, model: nn.Module, device="cpu") -> tuple[Tensor, Tensor]:
-    dataloader = DataLoader(dataset, batch_size=100)
-    model.eval()
-    ys = []
-    yhats = []
-    with torch.no_grad():
-        for X, y in dataloader:
-            X, y = X.to(device), y.to(device)
-            ys.append(y)
-            yhats.append(model(X))
-    return torch.concat(ys), torch.concat(yhats)
+    @torch.no_grad()
+    def predict(self, x: Tensor) -> Tensor:
+        self.eval()
+        dataloader = DataLoader(x, batch_size=100, shuffle=False)
+        return torch.concat([ self.forward(x_batch) for x_batch in dataloader ])
